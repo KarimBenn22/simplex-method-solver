3a05ab94031db52b72e2012e41cf89a484b0dee4
Karim <127413175+pywolf1503@users.noreply.github.com>
2024-12-23 13:04:21 +0000

    darkmode no longer forced

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-[family-name:var(--font-geist-sans)] dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-[family-name:var(--font-geist-sans)]`}
       >
         {children}
       </body>
